74a3d82b35433ee89ce010073bb5a1c397a651bc
Samuel <sammyokorie0@gmail.com>
2022-06-05 15:58:11 +0100

    fixed typo in dirname for homepage

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", function(req, res){
-    res.sendFile(__diherorname + "/signup.html");
+    res.sendFile(__dirname + "/signup.html");
 
 app.post("/", function(req, res){
     const firstname = req.body.fName;
